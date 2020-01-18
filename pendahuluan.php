9de2068eab413e620af36c6e7ea567d6ba2ac3dd
hafif07 <mhafif29@gmail.com>
2020-01-18 19:15:00 +0700

    add pendahuluan php

---
+++ pendahuluan.php
@@ -0,0 +1,35 @@
+<?php 
+
+//class 	
+// blueprint/template untuk membuat instance dari object
+// contoh denah rumah
+//class mendefinisikan object
+//menyimpan data dan prilaku yang disebut propery dan method
+
+//cara mendefinisikan class
+
+class NamaClass{
+	public $a;//property
+
+	public function b(){
+
+	}//method
+}
+
+
+// object
+// instance yang didefinisikan oleh class 
+// banyak object yang dibuat menggunakan satu class
+//object dibuat dengan  menggunakan keyword new
+
+$object1= new NamaClass();
+$object2= new NamaClass();
+
+
+var_dump($object1);
+var_dump($object2);
+
+
+
+
+
