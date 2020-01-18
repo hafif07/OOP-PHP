9e07d0745adcbca85bc80dc1411b50597c56e7af
hafif07 <mhafif29@gmail.com>
2020-01-18 20:43:12 +0700

    add properyMethod.php

---
+++ propertyMethod.php
@@ -0,0 +1,36 @@
+<?php 
+
+
+// property
+//merepresentasikan data / keadaan dari sebuah object
+//variabel yang berada di dalam object
+// sama seperti variabel di dalam  di php , ditambah didepan dengan visibiliy (public/private/protected)
+
+
+//method
+// mereperesentasikan prilaku dari sebuah object
+//function yang berada di dalam obejct
+// sama seperti function di dalam di php , ditambah didepan dengan visibiliy (public/private/protected)
+
+
+
+
+class Mobil{
+
+	public $warna;
+	public $merk;
+	public $jumlahPenumpang;
+
+	public function tambahKecepatan(){
+
+	}
+	public function kurangiKecepatan(){
+
+	}
+	public function gantiTransmisi(){
+
+	}
+
+}
+
+
